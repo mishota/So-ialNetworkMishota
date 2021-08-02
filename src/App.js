9eb47509f7b457e6358708583d3a03ed8ac2d218
Oksana Mishota <ksu_a@mail.ru>
2021-08-02 22:38:00 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://i.pinimg.com/736x/7f/a9/36/7fa9368444fec306b9fa2d61e4f4f3c2.jpg'/>
       </header>
+
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messeges</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+        <div></div>
+      </nav>
+
+      <div className='content'>
+        <div>
+          <img src='https://www.eyestudio.co.zw/wp-content/uploads/2018/12/Eye_Studio_New_Technology.jpg'/>
+        </div>
+        <div>
+          <img src='https://i.pinimg.com/736x/a8/98/76/a89876726813dc956ad3edb9faab8a13.jpg'/>
+          Description
+        </div>
+        <div>
+          MyPosts
+          <div>
+            NewPosts
+          </div>
+        </div>
+      </div>
+
+      
     </div>
   );
 }

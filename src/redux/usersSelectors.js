70c660f0873199444d3385ad9d7a89a1b2eb51fc
Oksana <32421051+mishota@users.noreply.github.com>
2021-10-07 17:29:58 +0300

    reselect commit

--- src/redux/usersSelectors.js
+++ src/redux/usersSelectors.js
@@ -1,6 +1,17 @@
+import { createSelector } from "reselect"
+
 export const getUsersFromState = (state) => {
    return state.userPage.users
 }
+
+// export const getFilterUsersFromState = (state) => {
+//    return getUsersFromState().filter(u => true)
+// }
+export const getUsersFromStateSUPER = createSelector((getUsersFromState, users) => {
+   return users.filter(u => true)
+})
+
+
 export const getPageSize = (state) => {
    return state.userPage.pageSize
 }
